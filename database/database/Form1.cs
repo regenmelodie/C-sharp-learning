82ed18b2867cdfec6078d02b2f5ec585be3fa7e2
guoyq16 <regenmelodie@163.com>
2020-08-09 19:26:02 +0800

    database connect

---
+++ database/database/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //第一：定义连接数据库的字符串：包含连接数据库的相关信息
+            string conStr = "server=127.0.0.1; port=3306; user=root; password=QJ&-(2cp*2ej; database=analysis_tool";
+
+            //第二：创建连接对象，并指定连接字符串
+            MySqlConnection conn = new MySqlConnection(conStr);
+
+            //第三：打开连接
+            conn.Open();
+
+            //第四：判断是否连接成功
+            if (conn.State==ConnectionState.Open)
+            {
+                MessageBox.Show("连接成功！");
+            }
+            else
+            {
+                MessageBox.Show("连接失败！");
+            }
+        }
+    }
+}
---
+++ datagridview01/datagridview01/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datagridview01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //第一：创建DataSet对象
+            DataSet set = new DataSet();
+            //第二：创建SQLDataAdapter对象
+            //1)定义连接对象
+            string dbstr = "server=.; uid=root; pwd=QJ&-(2cp*2ej"
+
+            //2)定义查询SQL语句
+
+            //第三：把查询的结果，填充到DataSet对象中
+
+            //第四：读取DataSet对象中的数据
+        }
+    }
+}
