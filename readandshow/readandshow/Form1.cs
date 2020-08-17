dc02c384babe48d1f33fd1b9604396dc608c0045
guoyq16 <regenmelodie@163.com>
2020-08-17 20:19:09 +0800

    读txt并展示

--- readandshow/readandshow/Form1.cs
+++ readandshow/readandshow/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace readandshow
 {
@@ -15,6 +16,34 @@ namespace readandshow
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //定义DataSet对象
+            DataSet set = new DataSet();
+            //创建一个表的对象
+            DataTable table = new DataTable();
+            set.Tables.Add(table);
+            //创建一个列的对象,并把列放入datatable
+            DataColumn dc = table.Columns.Add("column1", typeof(string));
+             
+            String path = "C:\\Users\\Regen\\Desktop\\C-sharp-learning\\htmlFiles\\strings.txt";
+            StreamReader sr = new StreamReader(path);
+            String line;
+
+            //创建行向表中填充数据
+            DataRow dr;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Console.WriteLine(line.ToString());
+                dr = table.NewRow();
+                dr["column1"] = line.ToString();
+                table.Rows.Add(dr);
+            }
+            
+            dataGridView1.DataSource = set.Tables[0];
+
         }
     }
 }
