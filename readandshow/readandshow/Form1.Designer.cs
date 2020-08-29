a250eb8318f44350f7f0af1e758a95279a1ee97c
guoyq16 <regenmelodie@163.com>
2020-08-29 16:30:40 +0800

    a

--- readandshow/readandshow/Form1.Designer.cs
+++ readandshow/readandshow/Form1.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +54,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "write HTML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 623);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -68,10 +80,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- readandshow/readandshow/Form1.cs
+++ readandshow/readandshow/Form1.cs
@@ -45,5 +45,53 @@ namespace readandshow
             dataGridView1.DataSource = set.Tables[0];
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "HTML File|*.html";
+            saveFileDialog.Title = "Save a HTML";
+            saveFileDialog.RestoreDirectory = true; //保存对话框是否记忆上次打开的目录
+            saveFileDialog.CheckPathExists = true; //检查目录
+            saveFileDialog.FileName = "UntitledHTML"; //设置默认文件名
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = saveFileDialog.FileName;
+                
+
+                MessageBox.Show(this, "保存成功！", "提示");
+            }
+
+
+            int rowSize = dataGridView1.RowCount;
+            for (int i=0; i<rowSize; i++)
+            {
+                
+            }
+
+            /*
+            WebBrowser webBrowser1 = new WebBrowser();
+            HtmlDocument doc = webBrowser1.Document;
+            HtmlElement html = doc.CreateElement("html");
+            doc.Body.AppendChild(html);
+
+            HtmlElement head = doc.CreateElement("head");
+            html.AppendChild(head);
+
+            HtmlElement meta = doc.CreateElement("meta");
+            meta.SetAttribute("http-equiv", "Content-Type");
+            meta.SetAttribute("content", "text/html");
+            meta.SetAttribute("charset", "UTF-8");
+            head.AppendChild(meta);
+            html.AppendChild(head);
+
+            HtmlElement body = doc.CreateElement("body");
+            HtmlElement div = doc.CreateElement("div");
+            div.InnerText = "123";
+            body.AppendChild(div);
+            html.AppendChild(body);
+            */
+            
+
+        }
     }
 }
